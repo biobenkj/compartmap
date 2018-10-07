40662a2dd86bb037d24867270385998176d5967d
Ben Johnson <ben.johnson4343@gmail.com>
2018-10-07 11:36:18 -0400

    Updating docs
    Former-commit-id: c3c697336643a3b48b8cda34a29b53827f24023c
    Former-commit-id: d3efd2e71d41557018cffec830b3cabf4a69c407
    Former-commit-id: d55c838668c611320239d9d7eb34b973e37398e6
    Former-commit-id: 68b3729b5285f0392bb73e1d4b033da7c7d91d68

--- R/getCompartments.R
+++ R/getCompartments.R
@@ -93,3 +93,29 @@ getCompartments <- function(obj, type = c("atac", "array"), res = 1e6, parallel 
     return(compartments)
   }
 }
+
+#' Example ATAC-seq data for compartmap
+#' 
+#' This data was generated using the data from the reference via bwa mem
+#' and pre-processing the data using the ATACseeker package.
+#' 
+#' @name filtered.data.chr14
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @references \url{https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP082417}
+#' @keywords data
+#' @usage data(filtered.data.chr14, package = "compartmap")
+NULL
+
+#' Example Illumina 450k methylation array data for compartmap
+#' 
+#' This data was generated using the data from the reference via the
+#' sesamize function from the SeSAMe package.
+#' 
+#' @name array.data.chr14
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @references \url{https://f1000research.com/articles/5-1281/v3}
+#' @keywords data
+#' @usage data(array.data.chr14, package = "compartmap")
+NULL
