8e5af8917f9713735e5d8f09a560e8d38fafbb91
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-28 18:27:31 -0500

    make plotAB use whatever mcol is requested
    Former-commit-id: f8db8d41d8345fc480d1aebd10084f28fb41c760
    Former-commit-id: 3a420f1d100cf26a8f39520324041716b31ec5ee
    Former-commit-id: 5ea8da860fb3eadd271555bf9b2284456cad3aad
    Former-commit-id: 620eb0cdb79ce65900ceb523599307b5b94ae117

--- R/plotAB.R
+++ R/plotAB.R
@@ -4,18 +4,17 @@
 #'
 #' @param x      The matrix obejct returned from getCompartments
 #' @param chr    Chromosome to subset to for plotting
+#' @param what   Which metadata column to plot
 #' @param main    Title for the plot
 #' @param ylim      Y-axis limits (default is -1 to 1)
-#' @param unitarize   Should the data be unitarized? (not explicitly necessary for arrays)
+#' @param unitarize   Should the data be unitarized? (not necessary for arrays)
 #' @param reverse    Reverse the sign of the PC values?
 #' @param top.col    Top (pos. PC values) chromatin color to be plotted
 #' @param bot.col    Bottom (neg. PC values) chromatin color to be plotted
 #' @param with.ci    Whether to plot confidence intervals
 #' @param filter    Whether to filter eigenvalues close to zero (default: TRUE)
-#' @param filter.lower.bound    The minimum absolute eigenvalue to include in the plot
-#' @param median.conf    Whether to plot the median confidence estimate across the chromosome
-#' 
-#' @return    invisibly, the compartment estimates from the plot
+#' @param filter.lower.bound  Minimum absolute eigenvalue to include in the plot
+#' @param median.conf Plot the median confidence estimate across the chromosome?
 #' 
 #' @import    GenomicRanges
 #' 
@@ -74,18 +73,24 @@
 
 
 
-plotAB <- function(x, chr = NULL, what = c("score", "flip.score"), main="",ylim=c(-1, 1),
-                   unitarize=FALSE, reverse=FALSE, top.col = "deeppink4", bot.col = "grey50",
-                   with.ci = FALSE, filter = TRUE, filter.min.eigen = 0.02,
-                   median.conf = FALSE){
+plotAB <- function(x, chr = NULL, what = "score", main="",ylim=c(-1, 1),
+                   unitarize=FALSE, reverse=FALSE, top.col = "deeppink4", 
+                   bot.col = "grey50", with.ci = FALSE, filter = TRUE, 
+                   filter.min.eigen = 0.02, median.conf = FALSE) {
+
   #what are we plotting
-  what <- match.arg(what)
-  #check if plotting CI
+  # what <- match.arg(what)
+  # (no, don't do that)
+
+  # check if plotting CI
+  # FIXME: refactor this 
   if (with.ci) {
     if (is(x, "GRanges")) {
       if (!is.null(chr)) x <- keepSeqlevels(x, chr, pruning.mode = "coarse")
       if (("conf.est" %in% names(mcols(x)))) {
-        if (filter) x <- x[abs(as(mcols(x)[what], "matrix")) > filter.min.eigen,]
+        if (filter) {
+          x <- x[abs(as(mcols(x)[what], "matrix")) > filter.min.eigen,]
+        }
         x.mat <- as(mcols(x)[what], "matrix")
         if (unitarize) x.mat <- .unitarize(x.mat)
         x.mat <- as.numeric(x.mat)
@@ -95,14 +100,11 @@ plotAB <- function(x, chr = NULL, what = c("score", "flip.score"), main="",ylim=
         col[x.mat<0] <- bot.col
         par(mar=c(1,5,1,1))
         par(mfrow=c(2,1))
-        barplot(x.mat, ylim=ylim, 
-                bty="n", xlab="", ylab="Eigenvector",border=col, col=col, main=main)
-        if (median.conf) {
-          barplot(x$conf.est, ylim=c(0,1), ylab="Compartment confidence estimates")
-          abline(h = median(x$conf.est), col = "red", lty = 2, lwd = 3)
-        } else {
-          barplot(x$conf.est, ylim=c(0,1), ylab="Compartment confidence estimates") 
-          }
+        barplot(x.mat, ylim=ylim, bty="n", xlab="", ylab="Eigenvector",
+                border=col, col=col, main=main)
+
+        barplot(x$conf.est, ylim=c(0,1), ylab="Compartment confidence estimate")
+        if (median.conf) abline(h=median(x$conf.est), col="red", lty=2, lwd=3)
 
       } else {
         message("conf.est isn't found in the mcols() of the input")
@@ -126,11 +128,13 @@ plotAB <- function(x, chr = NULL, what = c("score", "flip.score"), main="",ylim=
     n <- length(x)
     col <- rep(top.col, n)
     col[x<0] <- bot.col
-    barplot(x, ylim=ylim, 
-            bty="n", xlab="", ylab="Eigenvector", border=col, col=col, main=main)
-    }
+    barplot(x, ylim=ylim, bty="n", xlab="", ylab="Eigenvector", 
+            border=col, col=col, main=main)
   }
+}
 
+
+# helper fn
 .unitarize <- function(x, medianCenter = TRUE) {
   if (medianCenter) x <- x - median(x, na.rm = TRUE)
   bad <- is.na(x)
@@ -138,7 +142,8 @@ plotAB <- function(x, chr = NULL, what = c("score", "flip.score"), main="",ylim=
   n.bad <- sum(bad)
   if (n.bad > 0) {
     message(
-      sprintf("[.unitarize] %i missing values were ignored.\n", n.bad))
+      sprintf("[.unitarize] %i missing values were ignored.\n", n.bad)
+    )
   }
   x
 }
