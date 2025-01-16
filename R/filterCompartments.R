74a67479ebf3a80ab2d4876fee59b41af73c7a4e
James Eapen <james.eapen@vai.org>
2025-01-16 15:43:29 -0500

    chore(filterCompartments): style file

--- R/filterCompartments.R
+++ R/filterCompartments.R
@@ -1,5 +1,5 @@
 #' Filter compartments using confidence estimates and eigenvalue thresholds
-#' 
+#'
 #' @name filterCompartments
 #'
 #' @param obj Output of condenseSE or fixCompartments
@@ -11,29 +11,29 @@
 #' @export
 #'
 #' @examples
-#' 
 filterCompartments <- function(obj, min.conf = 0.7, min.eigen = 0.02) {
-  #filter compartments
-  message("Filtering compartments based on a minimum confidence of ", min.conf*100, "%")
+  # filter compartments
+  message("Filtering compartments based on a minimum confidence of ", min.conf * 100, "%")
   message("Filtering compartments based on a minimum absolute eigen value of ", min.eigen)
   if (is(obj, "list")) {
     filt.compartments <- lapply(obj, function(x) {
       filt <- filterer(x, min.conf, min.eigen)
-      return(x[filt,])
+      return(x[filt, ])
     })
   } else {
     filt <- filterer(obj, min.conf, min.eigen)
-    return(obj[filt,])
+    return(obj[filt, ])
   }
 }
 
 filterer <- function(obj, min.conf, min.eigen) {
   apply(mcols(obj), 1, function(x) {
-    #check if we have "fixed" things
+    # check if we have "fixed" things
     if ("flip.score" %in% names(x)) {
       filt.score <- ifelse(as.numeric(x["flip.conf.est"]) >= min.conf &
         abs(as.numeric(x["flip.score"])) >= min.eigen,
-        TRUE, FALSE)
+      TRUE, FALSE
+      )
       return(filt.score)
     } else {
       return(ifelse(as.numeric(x["conf.est"]) >= min.conf &
