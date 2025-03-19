e79ba7b3331eb2eabd6a783c825155cfc8d3bfcf
James Eapen <james.eapen@vai.org>
2025-03-19 12:32:54 -0400

    test(meanSmoother): test smoothing

---
+++ tests/testthat/test-meanSmoother.R
@@ -0,0 +1,23 @@
+test_that("meanSmoother", {
+  mat <- matrix(1:5, nrow = 5, ncol = 5)
+  expect_message(
+    meanSmoother(mat, k = 0),
+    "Returning unsmoothed x. This is probably an error.",
+    fixed = TRUE
+  )
+  expect_equal(
+    meanSmoother(mat, k = 0),
+    mat
+  )
+
+  # TODO get better error
+  expect_error(
+    meanSmoother(mat[1:2], k = 3),
+    "length(x) >= k is not TRUE",
+    fixed = TRUE
+  )
+})
+
+test_that(".meanSmoother.internal", {
+
+})
