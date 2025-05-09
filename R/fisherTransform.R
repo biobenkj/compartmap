1021a125a2506459782367b2bb2da00fa76ab955
James Eapen <james.eapen@vai.org>
2025-05-09 13:23:40 -0400

    style(fisherTransform,getCorMatrix): style with air

--- R/fisherTransform.R
+++ R/fisherTransform.R
@@ -1,9 +1,9 @@
 #' @title Fisher's Z transformation
 #'
-#' @description 
+#' @description
 #' \code{fisherZ} returns (squeezed) Fisher's Z transformed Pearson's r
 #'
-#' @details 
+#' @details
 #' This function returns (squeezed) Fisher's Z transformed Pearson's r
 #'
 #' @param cormat    Pearson correlation matrix
@@ -12,13 +12,13 @@
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' #Generate a random binary (-1, 1) matrix
 #' mat <- matrix(sample(c(1,-1), 10000, replace = TRUE), ncol = 100)
-#' 
+#'
 #' #Correct matrix diag
 #' diag(mat) <- 1
-#' 
+#'
 #' #Transform
 #' mat.transform <- fisherZ(mat)
 
@@ -29,10 +29,10 @@ fisherZ <- function(cormat) {
 
 #' @title Fisher's Z transformation
 #'
-#' @description 
+#' @description
 #' \code{fisherZ} returns the inverse (squeezed) Fisher's Z transformed Pearson's r. This will fail if a matrix is used as input instead of a vector.
 #'
-#' @details 
+#' @details
 #' This function returns the inverse (squeezed) Fisher's Z transformed Pearson's r
 #'
 #' @param cormat    vector of Fisher's Z transformed Pearson correlations or an eignevector
@@ -41,16 +41,16 @@ fisherZ <- function(cormat) {
 #' @export
 #'
 #' @examples
-#' 
+#'
 #' #Generate a random binary (-1, 1) matrix
 #' mat <- matrix(sample(c(1,-1), 10000, replace = TRUE), ncol = 100)
-#' 
+#'
 #' #Correct matrix diag
 #' diag(mat) <- 1
-#' 
+#'
 #' #Transform
 #' mat.transform <- fisherZ(mat)
-#' 
+#'
 #' #Back transform
 #' mat.transform.inverse <- apply(mat.transform, 1, ifisherZ)
 
