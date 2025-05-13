9c6f97478b845577a06b57a9ae880635d001f461
James Eapen <james.eapen@vai.org>
2025-05-13 16:53:53 -0400

    docs(fisherTransform): style fixes, use smaller, showable matrix for example

--- R/fisherTransform.R
+++ R/fisherTransform.R
@@ -1,60 +1,53 @@
-#' @title Fisher's Z transformation
+#' Fisher's Z transformation
 #'
-#' @description
-#' \code{fisherZ} returns (squeezed) Fisher's Z transformed Pearson's r
+#' `fisherZ` returns (squeezed) Fisher's Z transformed Pearson's r
 #'
-#' @details
-#' This function returns (squeezed) Fisher's Z transformed Pearson's r
+#' @param cormat Pearson correlation matrix
 #'
-#' @param cormat    Pearson correlation matrix
-#'
-#' @return    Fisher Z transformed Pearson correlations
+#' @return Fisher Z transformed Pearson correlations
 #' @export
 #'
 #' @examples
 #'
 #' #Generate a random binary (-1, 1) matrix
-#' mat <- matrix(sample(c(1,-1), 10000, replace = TRUE), ncol = 100)
+#' (mat <- matrix(sample(c(1,-1), 25, replace = TRUE), ncol = 5))
 #'
 #' #Correct matrix diag
 #' diag(mat) <- 1
 #'
 #' #Transform
-#' mat.transform <- fisherZ(mat)
-
+#' fisherZ(mat)
 fisherZ <- function(cormat) {
   if (any(cormat == 1)) cormat <- .squeezeit(cormat)
   atanh(cormat)
 }
 
-#Helper function to squeeze binary matrix for transformation
+# Helper function to squeeze binary matrix for transformation
 .squeezeit <- function(cormat) {
   cormat * 0.999999
 }
 
-#' @title Fisher's Z transformation
+#' Inverse Fisher's Z transformation
 #'
 #' `ifisherZ` returns the inverse (squeezed) Fisher's Z transformed
 #' Pearson's r.
 #'
-#' @details
-#' This function returns the inverse (squeezed) Fisher's Z transformed Pearson's r
 #' @param zmat matrix of Fisher's Z transformed Pearson correlations or an
 #' eignevector
 #'
-#' @return    Back transformed Fisher's Z
+#' @return Back transformed Fisher's Z Pearson correlations
 #' @export
 #'
 #' @examples
 #'
-#' #Generate a random binary (-1, 1) matrix
-#' mat <- matrix(sample(c(1,-1), 10000, replace = TRUE), ncol = 100)
+#' # Generate a random binary (-1, 1) matrix
+#' (mat <- matrix(sample(c(1,-1), 25, replace = TRUE), ncol = 5))
 #'
-#' #Correct matrix diag
+#' # Correct matrix diag
 #' diag(mat) <- 1
 #'
-#' #Transform
-#' mat.transform <- fisherZ(mat)
+#' # Transform
+#' (mat.transform <- fisherZ(mat))
 #'
 #' #Back transform
 #' ifisherZ(mat.transform)
